3541e7e831c8c8baf2589918e24e2548e38e1151
takamasa96 <black_white19930906@yahoo.co.jp>
2026-04-19 20:29:59 +0900

    40章まで

--- samuraitravel/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -1,13 +1,21 @@
 package com.example.samuraitravel.controller;
 
-import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.samuraitravel.entity.House;
+import com.example.samuraitravel.form.HouseRegisterForm;
 import com.example.samuraitravel.service.HouseService;
 
 @Controller
@@ -20,11 +28,44 @@ public class AdminHouseController {
     }
 
     @GetMapping
-    public String index(Model model) {
-        List<House> houses = houseService.findAllHouses();
+    public String index(@RequestParam(name = "keyword", required = false) String keyword,
+                        @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
+                        Model model)
+    {
+        Page<House> housePage;
 
-        model.addAttribute("houses", houses);
+        if (keyword != null && !keyword.isEmpty()) {
+            housePage = houseService.findHousesByNameLike(keyword, pageable);
+        } else {
+            housePage = houseService.findAllHouses(pageable);
+        }
+
+        model.addAttribute("housePage", housePage);
+        model.addAttribute("keyword", keyword);
 
         return "admin/houses/index";
     }
+    
+    @GetMapping("/{id}")
+    public String show(@PathVariable(name = "id") Integer id, RedirectAttributes redirectAttributes, Model model) {
+        Optional<House> optionalHouse  = houseService.findHouseById(id);
+
+        if (optionalHouse.isEmpty()) {
+            redirectAttributes.addFlashAttribute("errorMessage", "民宿が存在しません。");
+
+            return "redirect:/admin/houses";
+        }
+
+        House house = optionalHouse.get();
+        model.addAttribute("house", house);
+
+        return "admin/houses/show";
+    }    
+    
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("houseRegisterForm", new HouseRegisterForm());
+
+        return "admin/houses/register";
+    }   
 }
--- samuraitravel/src/main/java/com/example/samuraitravel/service/HouseService.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/service/HouseService.java
@@ -1,7 +1,9 @@
 package com.example.samuraitravel.service;
 
-import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.example.samuraitravel.entity.House;
@@ -15,8 +17,18 @@ public class HouseService {
         this.houseRepository = houseRepository;
     }
 
-    // すべての民宿を取得する
-    public List<House> findAllHouses() {
-        return houseRepository.findAll();
+    // すべての民宿をページングされた状態で取得する
+    public Page<House> findAllHouses(Pageable pageable) {
+        return houseRepository.findAll(pageable);
     }
+
+    // 指定されたキーワードを民宿名に含む民宿を、ページングされた状態で取得する
+    public Page<House> findHousesByNameLike(String keyword, Pageable pageable) {
+        return houseRepository.findByNameLike("%" + keyword + "%", pageable);
+    }
+    
+    // 指定したidを持つ民宿を取得する
+    public Optional<House> findHouseById(Integer id) {
+        return houseRepository.findById(id);
+    }    
 }
